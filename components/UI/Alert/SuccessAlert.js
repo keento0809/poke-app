108b0455d47e0a28c307759cc3e557ee49d58f07
KEENTO <handball0321@gmail.com>
2022-08-20 17:26:48 -0700

    Fix Alert

--- components/UI/Alert/SuccessAlert.js
+++ components/UI/Alert/SuccessAlert.js
@@ -9,7 +9,7 @@ const SuccessAlert = () => {
     <>
       <div
         id="alert-3"
-        className="flex w-full p-4 mx-auto my-4 bg-green-100 rounded-lg dark:bg-green-200"
+        className="flex w-full md:max-w-500 p-4 mr-auto my-4 bg-green-100 rounded-lg dark:bg-green-200"
         role="alert"
       >
         <svg
