0aa3b7805c811db3095d1cbb5294a8de20321d33
KEENTO <handball0321@gmail.com>
2023-01-31 17:22:27 -0800

    Solving the issue on deployment

---
+++ features/pokemonDetail/PokemonDetail.tsx
@@ -0,0 +1,37 @@
+import { useRouter } from "next/router";
+import { useContext } from "react";
+import { AppContext } from "../../context/state";
+import Image, { StaticImageData } from "next/image";
+
+type Props = {
+  pokemonId: number;
+  type: string;
+  image: string;
+};
+
+function PokemonDetail({ pokemonId, type, image }: Props) {
+  const { handleLoading } = useContext(AppContext);
+  const router = useRouter();
+
+  const handleJumpToPokemonDetailPage = () => {
+    handleLoading(true);
+    router.push(`/pokemon/${pokemonId}`);
+  };
+
+  return (
+    <div
+      onClick={handleJumpToPokemonDetailPage}
+      className={`cursor-pointer flex justify-center items-center rounded-lg pt-2 pb-4 hover:scale-110 transition-transform ${type}}`}
+    >
+      <img
+        src={`${image}`}
+        alt=""
+        width="100px"
+        height="100px"
+        className="mx-auto"
+      />
+    </div>
+  );
+}
+
+export default PokemonDetail;
