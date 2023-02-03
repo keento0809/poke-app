ac48bc906854bc853019e434245ed2bdfbc68840
KEENTO <handball0321@gmail.com>
2023-02-03 14:03:12 -0800

    Fixed click function on buttons in PokemonDetail.tsx

--- context/state.tsx
+++ context/state.tsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useState } from "react";
 import { AppContextType } from "../types/appContext";
 
 export const AppContext = createContext<AppContextType | null>(null);
